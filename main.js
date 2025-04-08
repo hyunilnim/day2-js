422d3434ad6522f7a710d8c23459aa05a7fd4918
hyunilnim <gusdlf2357@naver.com>
2025-04-08 17:41:35 +0900

    click rotate(web motion) event2

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-const [btn1, btn2] = document.querySelectorAll('button');
-const box = document.querySelector('.box');
+const btnCall = document.querySelector(".btnCall");
+const btnClose = document.querySelector(".btnClose");
+const mobileMenu = document.querySelector(".mobileMenu");
 
-let count = 0;
+//메뉴 활성화 처리 이벤트
+btnCall.addEventListener("click", () => {
+  mobileMenu.classList.add("on");
+});
 
-btn2.addEventListener('click', ()=>{
-  
-  box.style.transform = `rotate(${30* ++count}deg`
-})
-
-btn1.addEventListener('click', ()=>{
-  
-  box.style.transform = `rotate(${30* --count}deg`
-})
+//메뉴 비활성화 처리 이벤트
+btnClose.addEventListener("click", () => {
+  mobileMenu.classList.remove("on");
+});
